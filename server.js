dfbcb87108a9b95ae48fc21dc5d18fc4d10c44b7
Syed Umar Farooq <umar@example.com>
2025-07-17 19:03:20 +0530

    Fix case issue for User model

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const dotenv = require('dotenv');
-const User = require('./models/user'); // Import User model
+const User = require('./models/User'); // Import User model
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
